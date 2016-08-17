0e099e6a5320eb6dc9ac181517c54722941efa42
Ankur Aggarwal <roc.windwarrior@gmail.com>
2016-08-17 15:11:56 +0530

    Code to fetch joke from the GCE. Displaying toast right now

--- gcejokeprovider/src/main/java/com/aggarwalankur/gce/jokeprovider/MyEndpoint.java
+++ gcejokeprovider/src/main/java/com/aggarwalankur/gce/jokeprovider/MyEndpoint.java
@@ -10,6 +10,10 @@ import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import javax.inject.Named;
 
 /**
@@ -26,6 +30,28 @@ import javax.inject.Named;
 )
 public class MyEndpoint {
 
+    private List<String> mJokesArchive;
+    private int mJokesArchiveSize;
+    private static Random mRandom;
+
+    public MyEndpoint(){
+        mJokesArchive = new ArrayList<>();
+
+        mJokesArchive.add("This is a joke");
+        mJokesArchive.add("This is a funny joke");
+        mJokesArchive.add("This is a hilarious joke");
+        mJokesArchive.add("This is a totally funny joke");
+        mJokesArchive.add("This is a lol joke!");
+        mJokesArchive.add("This is a rofl joke!");
+        mJokesArchive.add("This is a bone-tickling joke !!");
+        mJokesArchive.add("This is a poor joke. :(");
+        mJokesArchive.add("This is a knock-knock joke :)");
+
+        mJokesArchiveSize = mJokesArchive.size();
+
+        mRandom = new Random();
+    }
+
     /**
      * A simple endpoint method that takes a name and says Hi back
      */
@@ -33,6 +59,17 @@ public class MyEndpoint {
     public MyBean sayHi(@Named("name") String name) {
         MyBean response = new MyBean();
         response.setData("Hi, " + name);
+
+        return response;
+    }
+
+    /**
+     * Method to get a random joke from the list of jokes
+     */
+    @ApiMethod(name = "getJoke")
+    public MyBean getJoke(){
+        MyBean response = new MyBean();
+        response.setData(mJokesArchive.get(mRandom.nextInt(mJokesArchiveSize)));
 
         return response;
     }
