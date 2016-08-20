de40ecb2f8f0f8a2e4d9387743a5b0a48f68b280
Ankur Aggarwal <roc.windwarrior@gmail.com>
2016-08-20 23:04:18 +0530

    Add 1st test

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
@@ -3,11 +3,53 @@ package com.udacity.gradle.builditbigger;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import com.udacity.gradle.builditbigger.communication.EndpointsAsyncTask;
+
+import java.util.concurrent.CountDownLatch;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+
+    String mFetchedJoke = null;
+    Exception mError = null;
+    CountDownLatch mCountDownLatch = null;
+
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        mCountDownLatch = new CountDownLatch(1);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        mCountDownLatch.countDown();
+    }
+
+    public void testGCEAsyncTask() throws InterruptedException {
+
+        EndpointsAsyncTask task = new EndpointsAsyncTask();
+        try{
+            task.execute(new EndpointsAsyncTask.OnJokeFetchedListener() {
+                @Override
+                public void OnJokeFetched(String loadedJoke) {
+                    mFetchedJoke = loadedJoke;
+                    mCountDownLatch.countDown();
+                }
+            });
+        }catch (Exception e){
+            mError = e;
+        }
+
+        mCountDownLatch.await();
+
+        assertNull(mError);
+        assertNotNull(mFetchedJoke);
+        assertTrue(!mFetchedJoke.isEmpty());
+
     }
 }
